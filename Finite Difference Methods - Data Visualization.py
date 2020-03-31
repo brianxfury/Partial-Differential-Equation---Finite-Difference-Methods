b21429f4ffbc8a6b78c52ecea29391934640f7ba
Bryan D <46956022+brianxfury@users.noreply.github.com>
2020-03-31 22:28:46 +0800

    Create Finite Difference Methods - Data Visualization.py
    The finite-difference method can easily be extended to higher dimensions by using
    the finite-difference formula along each discretized coordinate. For a two-dimensional
    problem, we have a two-dimensional array u for the unknown interior function values,
    and when using the finite differential formula, we obtain a system of coupled equations
    for the elements in u. To write these equations on the standard matrix-vector form, we
    can rearrange the u array into a vector and assemble the corresponding matrix A from
    the finite-difference equations.

---
+++ Finite Difference Methods - Data Visualization.py
@@ -0,0 +1,31 @@
+# Scientific Computing using Python 
+# @Bryan Dimabayao
+# Partial Differential Equation - Finite Difference Methods
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import mpl_toolkits.mplot3d
+import scipy.sparse as sp
+import scipy.sparse.linalg
+import scipy.linalg as la
+import dolfin
+import mshr
+
+N = 5
+u0, u1 = 1, 2
+dx = 1.0 / (N + 1)
+
+A = (np.eye(N, k=-1) - 2 * np.eye(N) + np.eye(N, k=1)) / dx**2
+A
+#Output of Array
+
+x = np.linspace(0, 1, N+2)
+U = np.hstack([[u0], u, [u1]])
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.plot(x, U)
+ax.plot(x[1:-1], u, 'ks')
+ax.set_xlim(0, 1)
+ax.set_xlabel(r"$x$", fontsize=18)
+ax.set_ylabel(r"$u(x)$", fontsize=18)
+# Plot of Solution to the second-order ODE boundary value problem
